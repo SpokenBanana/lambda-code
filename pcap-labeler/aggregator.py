e8788dc603e071b092b983701946e516fa4ce759
David Santana <santana21796@me.com>
2018-01-17 22:32:44 -0500

    Added aggregation for new features

--- pcap-labeler/aggregator.py
+++ pcap-labeler/aggregator.py
@@ -6,9 +6,20 @@ be used for machine learning.
 """
 import os
 from datetime import datetime, timedelta
+from collections import Counter
+import numpy
 
 
 PCAP_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+
+
+def entropy(ctr):
+    a = ctr.values()
+    b = sum(a)
+    al = numpy.asarray(a)
+    al = al / float(b)
+    return -sum(numpy.log(al) * al)
+
 
 class Summary:
 
@@ -26,17 +37,29 @@ class Summary:
         self.n_icmp = 0
         self.label = False
 
+        self.ws_counter = Counter()
+        self.hs_counter = Counter()
+        self.pnd_counter = Counter()
+        self.pns_counter = Counter()
+        self.tcp_flag_counter = Counter()
+
     def add(self, features):
         self.packet_count += 1
         self.average_packet += (
-                features['packet_length']
-                - self.average_packet) / self.packet_count
+            features['packet_length'] -
+            self.average_packet) / self.packet_count
 
         self.ttl_count += 1
         self.average_ttl += (
-                features['TTL'] - self.average_ttl) / self.packet_count
+            features['TTL'] - self.average_ttl) / self.packet_count
 
         self.average_fragment_count += 1
+
+        self.ws_counter[features['ws']] += 1
+        self.hs_counter[features['hs']] += 1
+        self.pns_counter[features['pns']] += 1
+        self.pnd_counter[features['pnd']] += 1
+        self.tcp_flag_counter[features['tcp_flag']] += 1
 
         proto = int(features['proto'])
 
@@ -50,7 +73,7 @@ class Summary:
         if features['label'] == 'Botnet':
             self.label = True
 
-    def to_feature_array():
+    def to_feature_array(self):
         return [
             self.n_udp,
             self.n_tcp,
@@ -58,6 +81,11 @@ class Summary:
             self.average_packet,
             self.average_ttl,
             self.average_fragment_count,
+            entropy(self.ws_counter),
+            entropy(self.hs_counter),
+            entropy(self.pnd_counter),
+            entropy(self.pns_counter),
+            entropy(self.tcp_flag_counter),
             'Botnet' if self.label else 'Normal'
         ]
 
@@ -66,18 +94,27 @@ def write_aggregated_file(filename, directory):
     features = []
     basename = filename.split('.')[0].split('/')[1]
     with open(filename, 'r+') as f:
-        with open(directory + '/'+basename + '.featureset.csv',
-                'w+') as out:
+        with open(
+                directory + '/'+basename + '.featureset.csv', 'w+') as out:
             headers = f.readline().strip().split(',')
             last = None
             bound = None
             current = list(range(4))
             feats = [
-                'proto',
-                'packet_length',
-                'fragment_offset',
-                'TTL'
+                'n_udp',
+                'n_tcp',
+                'n_icmp',
+                'average_packet',
+                'average_ttl',
+                'average_fragment_count',
+                'entropy_ws',
+                'entropy_hs',
+                'entropy_pnd',
+                'entropy_pns',
+                'entropy_tcp_flag',
+                'label'
             ]
+            out.write(','.join(feats) + '\n')
             summary = Summary()
             for line in f:
                 info = line.strip().split(',')
